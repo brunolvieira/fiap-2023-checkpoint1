d45615b062f8c4afffeb3faf01c7ae9a4c4f1714
brunolvieira <bruno.l.vieira@hotmail.com>
2023-03-16 20:40:30 -0300

    feat: configurações de launch no vscode

--- checkpoint1/src/main/java/br/com/fiap/checkpoint1/Repository/EmpregadoRepository.java
+++ checkpoint1/src/main/java/br/com/fiap/checkpoint1/Repository/EmpregadoRepository.java
@@ -1,11 +1,9 @@
 package br.com.fiap.checkpoint1.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import br.com.fiap.checkpoint1.Model.EmpregadoModel;
 
-@Repository
 public interface EmpregadoRepository extends JpaRepository<EmpregadoModel, Long> {
     
 }
